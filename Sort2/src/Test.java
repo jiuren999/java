cb2fd03feb2206c3c8e7445e4de0b502110b2c3c
bai-zi <2505455179@qq.com>
2023-07-26 21:38:04 +0800

    排序算法全

---
+++ Sort2/src/Test.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int[] arr = {9,7,5,3,4,1,2};
+        Sort.mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+//
+//        Sort.shellSort(arr);
+//        System.out.println(Arrays.toString(arr));
+//        Sort.selectSort2(arr);
+//        Sort.bubbleSort(arr);
+//        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
